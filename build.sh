53ffdd1325611d81a8a2ba7e1e319ee2ffc1ebfc
F1L1P <78918286+F1L1Pv2@users.noreply.github.com>
2025-02-05 12:24:18 +0100

    fully finished

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 set -xe
 
-clang++ -std=c++17 -g main.cpp -o main.exe 3dmath.cpp -fopenmp
+clang++ -std=c++17 -g main.cpp -o main.exe 3dmath.cpp -fopenmp -Wno-unused-value
 ./main.exe
--- main.cpp
+++ main.cpp
@@ -14,8 +14,14 @@ using namespace std;
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
 
-#define IMAGE_WIDTH 128
-#define IMAGE_HEIGHT 128
+#define STB_IMAGE_RESIZE_IMPLEMENTATION
+#include "stb_image_resize.h"
+
+#define IMAGE_WIDTH 32
+#define IMAGE_HEIGHT 32
+#define INNER_SIZE 256
+#define SMOOTH_VAL (0.15f)
+
 uint32_t* pixels;
 
 double cross(const vec2& a, const vec2& b){
@@ -549,7 +555,7 @@ float smoothStep(float edge0, float edge1, float x) {
 }
 
 float smoothDistance(float distance) {
-    return smoothStep(-0.05f, 0.05f, distance);
+    return smoothStep(-SMOOTH_VAL, SMOOTH_VAL, distance);
     // return smoothStep(-1.0f, 1.0f, distance) > 0.5 ? 1.0f : 0.0f;
     // return (distance+1)/2;
 }
@@ -588,7 +594,7 @@ float median(float a, float b, float c) {
 }
 
 void collisionCorrection(uint32_t* pixels, int width, int height) {
-    const float threshold = 0.99f;
+    const float threshold = 0.2f;
 
     for (int y = 1; y < height - 1; ++y) {
         for (int x = 1; x < width - 1; ++x) {
@@ -629,47 +635,46 @@ void collisionCorrection(uint32_t* pixels, int width, int height) {
 }
 
 int main() {
-    pixels = (uint32_t*)malloc(IMAGE_WIDTH * IMAGE_HEIGHT * 4);
 
     // random shape
 
-    vec2 Offsetter = {0.01f,0.5f};
-    vec2 a  = { Offsetter.x+0.00f,Offsetter.y+0.0f};
-    vec2 aI = { Offsetter.x+0.20f,Offsetter.y+0.0f};
-    vec2 b  = { Offsetter.x+0.40f,Offsetter.y+0.2f};
-    vec2 bI = { Offsetter.x+0.60f,Offsetter.y+0.4f};
-    vec2 c  = {-Offsetter.x+1.00f,Offsetter.y+0.3f};
-    vec2 cI = { Offsetter.x+0.70f,Offsetter.y+0.26f};
-    vec2 d  = { Offsetter.x+0.75f,Offsetter.y+0.05f};
-    vec2 dI = { Offsetter.x+0.80f,Offsetter.y+-0.16f};
-    vec2 e  = {-Offsetter.x+1.00f,Offsetter.y+-0.1f};
-    vec2 eI = { Offsetter.x+0.80f,Offsetter.y+-0.25f};
-    vec2 f =  { Offsetter.x+0.50f,Offsetter.y+-0.3f};
-    vec2 fI = { Offsetter.x+0.20f,Offsetter.y+-0.3f};
+    // vec2 Offsetter = {0.01f,0.5f};
+    // vec2 a  = { Offsetter.x+0.00f,Offsetter.y+0.0f};
+    // vec2 aI = { Offsetter.x+0.20f,Offsetter.y+0.0f};
+    // vec2 b  = { Offsetter.x+0.40f,Offsetter.y+0.2f};
+    // vec2 bI = { Offsetter.x+0.60f,Offsetter.y+0.4f};
+    // vec2 c  = {-Offsetter.x+1.00f,Offsetter.y+0.3f};
+    // vec2 cI = { Offsetter.x+0.70f,Offsetter.y+0.26f};
+    // vec2 d  = { Offsetter.x+0.75f,Offsetter.y+0.05f};
+    // vec2 dI = { Offsetter.x+0.80f,Offsetter.y+-0.16f};
+    // vec2 e  = {-Offsetter.x+1.00f,Offsetter.y+-0.1f};
+    // vec2 eI = { Offsetter.x+0.80f,Offsetter.y+-0.25f};
+    // vec2 f =  { Offsetter.x+0.50f,Offsetter.y+-0.3f};
+    // vec2 fI = { Offsetter.x+0.20f,Offsetter.y+-0.3f};
 
     // AAAAAAAAAA
 
-    vec2 p1 = {0.1 *0.4 + 0.3,0.1 * 0.4 + 0.3};
-    vec2 p2 = {0.3 *0.4 + 0.3,0.9 * 0.4 + 0.3};
-    vec2 p3 = {0.7 *0.4 + 0.3,0.9 * 0.4 + 0.3};
-    vec2 p4 = {0.9 *0.4 + 0.3,0.1 * 0.4 + 0.3};
-    vec2 p5 = {0.7 *0.4 + 0.3,0.1 * 0.4 + 0.3};
-    vec2 p6 = {0.6 *0.4 + 0.3,0.4 * 0.4 + 0.3};
-    vec2 p7 = {0.4 *0.4 + 0.3,0.4 * 0.4 + 0.3};
-    vec2 p8 = {0.3 *0.4 + 0.3,0.1 * 0.4 + 0.3};
+    // vec2 p1 = {0.1,0.1};
+    // vec2 p2 = {0.3,0.9};
+    // vec2 p3 = {0.7,0.9};
+    // vec2 p4 = {0.9,0.1};
+    // vec2 p5 = {0.7,0.1};
+    // vec2 p6 = {0.6,0.4};
+    // vec2 p7 = {0.4,0.4};
+    // vec2 p8 = {0.3,0.1};
 
     // EEEEEEEEE
 
-    // vec2 p1 = {0.106,0.486};
-    // vec2 p2 = {0.1,0.9};
-    // vec2 p3 = {0.5,0.9};
-    // vec2 p4 = {0.8,0.9};
-    // vec2 p5 = {0.8,0.5};
-    // vec2 p6 = {0.3,0.5};
-    // vec2 p7 = {0.295,0.24};
-    // vec2 p8 = {0.8,0.3};
-    // vec2 p9 = {0.8,0.15};
-    // vec2 p10 = {0.1,0.05};
+    vec2 p1 = {0.106,0.486};
+    vec2 p2 = {0.1,0.9};
+    vec2 p3 = {0.5,0.9};
+    vec2 p4 = {0.8,0.9};
+    vec2 p5 = {0.8,0.5};
+    vec2 p6 = {0.3,0.5};
+    vec2 p7 = {0.295,0.24};
+    vec2 p8 = {0.8,0.3};
+    vec2 p9 = {0.8,0.15};
+    vec2 p10 = {0.1,0.05};
 
     // reverse triangle
     // vec2 tp1 = {0.3,0.5};
@@ -677,9 +682,9 @@ int main() {
     // vec2 tp3 = {0.5,0.85};
 
     // reverse triangle-e
-    // vec2 tp1 = {0.3,0.55};
-    // vec2 tp2 = {0.7,0.55};
-    // vec2 tp3 = {0.5,0.8};
+    vec2 tp1 = {0.3,0.55};
+    vec2 tp2 = {0.7,0.55};
+    vec2 tp3 = {0.5,0.8};
 
 
     Shape test{
@@ -689,36 +694,36 @@ int main() {
 
                     // random shape
 
-                    Edge(EdgeBezier{
-                        a,
-                        b,
-                        aI
-                    }),
-                    Edge(EdgeBezier{
-                        b,
-                        c,
-                        bI
-                    }),
-                    Edge(EdgeBezier{
-                        c,
-                        d,
-                        cI
-                    }),
-                    Edge(EdgeBezier{
-                        d,
-                        e,
-                        dI
-                    }),
-                    Edge(EdgeBezier{
-                        e,
-                        f,
-                        eI
-                    }),
-                    Edge(EdgeBezier{
-                        f,
-                        a,
-                        fI
-                    }),
+                    // Edge(EdgeBezier{
+                    //     a,
+                    //     b,
+                    //     aI
+                    // }),
+                    // Edge(EdgeBezier{
+                    //     b,
+                    //     c,
+                    //     bI
+                    // }),
+                    // Edge(EdgeBezier{
+                    //     c,
+                    //     d,
+                    //     cI
+                    // }),
+                    // Edge(EdgeBezier{
+                    //     d,
+                    //     e,
+                    //     dI
+                    // }),
+                    // Edge(EdgeBezier{
+                    //     e,
+                    //     f,
+                    //     eI
+                    // }),
+                    // Edge(EdgeBezier{
+                    //     f,
+                    //     a,
+                    //     fI
+                    // }),
 
                     // AAAAAAAAAAA
                     // Edge(EdgeLine{
@@ -805,121 +810,121 @@ int main() {
                     // ),
 
                     // EEEEEEEEEEEE
-                    // Edge(EdgeBezier{
-                    //     p9,
-                    //     p1,
-                    //     p10
-                    // }),
-                    // Edge(EdgeBezier{
-                    //     p1,
-                    //     p3,
-                    //     p2
-                    // }),
-                    // Edge(EdgeBezier{
-                    //     p3,
-                    //     p5,
-                    //     p4
-                    // }),
-                    // Edge(EdgeBezier{
-                    //     p5,
-                    //     p6,
-                    //     p5
-                    // }),
-                    // Edge(EdgeBezier{
-                    //     p6,
-                    //     p8,
-                    //     p7
-                    // }),
-                    // Edge(EdgeBezier{
-                    //     p8,
-                    //     p9,
-                    //     p8
-                    // }),
+                    Edge(EdgeBezier{
+                        p9,
+                        p1,
+                        p10
+                    }),
+                    Edge(EdgeBezier{
+                        p1,
+                        p3,
+                        p2
+                    }),
+                    Edge(EdgeBezier{
+                        p3,
+                        p5,
+                        p4
+                    }),
+                    Edge(EdgeBezier{
+                        p5,
+                        p6,
+                        p5
+                    }),
+                    Edge(EdgeBezier{
+                        p6,
+                        p8,
+                        p7
+                    }),
+                    Edge(EdgeBezier{
+                        p8,
+                        p9,
+                        p8
+                    }),
                 }
             },
 
             Contour {
                 {
                     // AAAAAAAAAAAAA - reverse winding
-                    Edge(EdgeLine{
-                            p1,
-                            p8
-                        }
-                    ),
-                    Edge(EdgeLine{
-                            p8,
-                            p7
-                        }
-                    ),
-                    Edge(EdgeLine{
-                            p7,
-                            p6
-                        }
-                    ),
-                    Edge(EdgeLine{
-                            p6,
-                            p5
-                        }
-                    ),
-                    Edge(EdgeLine{
-                            p5,
-                            p4
-                        }
-                    ),
-                    Edge(EdgeLine{
-                            p4,
-                            p3
-                        }
-                    ),
-                    Edge(EdgeLine{
-                            p3,
-                            p2
-                        }
-                    ),
-                    Edge(EdgeLine{
-                            p2,
-                            p1
-                        }
-                    ),
+                    // Edge(EdgeLine{
+                    //         p1,
+                    //         p8
+                    //     }
+                    // ),
+                    // Edge(EdgeLine{
+                    //         p8,
+                    //         p7
+                    //     }
+                    // ),
+                    // Edge(EdgeLine{
+                    //         p7,
+                    //         p6
+                    //     }
+                    // ),
+                    // Edge(EdgeLine{
+                    //         p6,
+                    //         p5
+                    //     }
+                    // ),
+                    // Edge(EdgeLine{
+                    //         p5,
+                    //         p4
+                    //     }
+                    // ),
+                    // Edge(EdgeLine{
+                    //         p4,
+                    //         p3
+                    //     }
+                    // ),
+                    // Edge(EdgeLine{
+                    //         p3,
+                    //         p2
+                    //     }
+                    // ),
+                    // Edge(EdgeLine{
+                    //         p2,
+                    //         p1
+                    //     }
+                    // ),
                 }
             },
 
-            // Contour{
-            //     {
-            //         // triangle
-            //         // Edge(EdgeLine{
-            //         //         tp1,
-            //         //         tp3
-            //         //     }
-            //         // ),
-            //         // Edge(EdgeLine{
-            //         //         tp3,
-            //         //         tp2
-            //         //     }
-            //         // ),
-            //         // Edge(EdgeLine{
-            //         //         tp2,
-            //         //         tp1
-            //         //     }
-            //         // ),
-            //         // triangle reversed
-            //         Edge(EdgeLine{
-            //                 tp1,
-            //                 tp2
-            //             }
-            //         ),
-            //         Edge(EdgeLine{
-            //                 tp2,
-            //                 tp3
-            //             }
-            //         ),
-            //         Edge(EdgeLine{
-            //                 tp3,
-            //                 tp1
-            //             }
-            //         ),
-            //     }
-            // }
+            Contour{
+                {
+                    // triangle
+                    // Edge(EdgeLine{
+                    //         tp1,
+                    //         tp3
+                    //     }
+                    // ),
+                    // Edge(EdgeLine{
+                    //         tp3,
+                    //         tp2
+                    //     }
+                    // ),
+                    // Edge(EdgeLine{
+                    //         tp2,
+                    //         tp1
+                    //     }
+                    // ),
+                    // triangle reversed
+                    Edge(EdgeLine{
+                            tp1,
+                            tp2
+                        }
+                    ),
+                    Edge(EdgeLine{
+                            tp2,
+                            tp3
+                        }
+                    ),
+                    Edge(EdgeLine{
+                            tp3,
+                            tp1
+                        }
+                    ),
+                }
+            }
         }
     };
 
@@ -930,21 +935,34 @@ int main() {
 
     test.edgeColoring();
 
+    pixels = (uint32_t*)malloc(INNER_SIZE * INNER_SIZE * 4);
+
+    uint32_t* outPixels;
+
     #pragma omp parallel for
-    for(int y = 0; y < IMAGE_HEIGHT; y++){
-        for(int x = 0; x < IMAGE_WIDTH; x++){
-            vec2 P = {((float)x+0.5f)/IMAGE_WIDTH, ((float)y+0.5f)/IMAGE_HEIGHT};
-            pixels[(IMAGE_HEIGHT-1-y)*IMAGE_WIDTH+x] = generatePixel(test,P);
-        }
+    for(int i = 0; i < INNER_SIZE*INNER_SIZE; i++){
+        int x = i % INNER_SIZE;
+        int y = INNER_SIZE - i / INNER_SIZE;
+        vec2 P = {((float)x+0.5f)/INNER_SIZE, ((float)y+0.5f)/INNER_SIZE};
+        pixels[i] = generatePixel(test,P);
     }
 
-    collisionCorrection(pixels, IMAGE_WIDTH, IMAGE_HEIGHT);
 
-    // test.draw(0xFFFFFFFF, 3); // White color, thickness = 3
+    if(IMAGE_WIDTH == INNER_SIZE && IMAGE_HEIGHT == INNER_SIZE){
+        outPixels = pixels;
+    }else{
+        outPixels = (uint32_t*)malloc(IMAGE_WIDTH * IMAGE_HEIGHT * 4);
+        stbir_resize_uint8((const unsigned char*)pixels,INNER_SIZE,INNER_SIZE,INNER_SIZE*4,(unsigned char*)outPixels,IMAGE_WIDTH,IMAGE_HEIGHT,IMAGE_WIDTH*4,4);
+    }
 
-    if(!stbi_write_png("out.png", IMAGE_WIDTH, IMAGE_HEIGHT, 4, pixels, IMAGE_WIDTH * 4)){
+    collisionCorrection(outPixels, IMAGE_WIDTH, IMAGE_HEIGHT);
+
+    if(!stbi_write_png("out.png", IMAGE_WIDTH, IMAGE_HEIGHT, 4, outPixels, IMAGE_WIDTH * 4)){
         printf("Could not save");
     }
+
+    if(outPixels != pixels) free(outPixels);
+    free(pixels);
 
     return 0;
 }
